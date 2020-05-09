daeb66914f7f44dbab6ae682dd3daa8d11069ebf
BandhaviC <bandhavic6@gmail.com>
2020-05-09 11:21:24 -0700

    commit changes

--- src/main.ts
+++ src/main.ts
@@ -3,31 +3,10 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
-import { start } from "repl";
 
 if (environment.production) {
   enableProdMode();
 }
-const beginNum = document.getElementById("numberInput");
-const endNum = document.getElementById("numberInput1");
-const button = document.getElementById("submit");
-button.addEventListener("click", start);
-
-function number_even(x, y) {
-  var numbers: number[];
-
-  if (x > y) {
-    for (let i = y + 1; i < x; i++) {
-      if (i % 2 == 0) {
-        numbers.push(i);
-      }
-    }
-  }
-
-  return numbers;
-}
-var result: any;
-result = numbers.join(",");
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
